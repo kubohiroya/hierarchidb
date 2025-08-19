bdd6840416e4245293a6c38e0d20217b44e95783
Hiroya Kubo <hiroya@cuc.ac.jp>
2025-08-20 08:20:16 +0900

    Updates

---
+++ assets/Folder-CIpXYxUK.js
@@ -0,0 +1,1 @@
+import{j as h}from"./jsx-runtime-D_zvdyIk.js";import{c}from"./Box-zDrc8-aq.js";const s=c(h.jsx("path",{d:"M22 11V3h-7v3H9V3H2v8h7V8h2v10h4v3h7v-8h-7v3h-2V8h2v3z"})),r=c(h.jsx("path",{d:"M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8z"}));export{s as A,r as F};
---
+++ assets/RestoreFromTrash-C4zCvTlp.js
@@ -0,0 +1,1 @@
+import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{c as t}from"./Box-zDrc8-aq.js";const c=t(o.jsx("path",{d:"M19 4h-3.5l-1-1h-5l-1 1H5v2h14zM6 7v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7zm8 7v4h-4v-4H8l4-4 4 4z"}));export{c as R};
