ad4e71ab2e9533d1ba3c08d91031c96a61282b3a
Hiroya Kubo <hiroya@cuc.ac.jp>
2025-09-03 19:38:40 +0900

    chore(worker): remove deprecated packages/runtime-worker/worker/_src\n\n- No references remain.\n- Package typechecks and builds clean locally.

--- packages/runtime-worker/worker/_src/handlers/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseEntityHandler } from './BaseEntityHandler';
-// Temporarily disabled - these handlers need to be updated to new API
-// export { PeerEntityHandler } from './PeerEntityHandler';
-// export { GroupEntityHandler } from './GroupEntityHandler';
-// export { RelationalEntityHandler } from './RelationalEntityHandler';
-export * from './types';
-// export { WorkingCopyHandler } from './WorkingCopyHandler';
